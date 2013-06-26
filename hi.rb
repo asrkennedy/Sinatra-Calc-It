09eedc45c8a94b0cf373076a337a7deb08ccb5fa
Andrea Kennedy <asrkennedy@gmail.com>
2013-06-26 19:23:38 +0100

    all calc included sinatra

--- hi.rb
+++ hi.rb
@@ -2,30 +2,74 @@ require 'pry'
 require 'sinatra'
 require 'sinatra/contrib/all'
 
-get '/hi' do
-  "Hello World!"
-end
-
 get '/' do
-  "this is the homepage"
+  erb :home
+end
+
+get '/basic_calc' do
+  @first = params[:first].to_f
+  @second = params[:second].to_f
+  @operator = params[:operator]
+
+  @result = case @operator
+                when "+" then @first + @second
+                when "-" then @first = @second
+                when "*" then @first * @second
+                when "/" then @first / @second
+              end
+
+  erb :basic_calc
+end
+
+get '/bmi_calc' do
+  @measurement = params[:measurement]
+  @weight = params[:weight].to_f
+  @height = params[:height].to_f
+
+  @result = case @measurement
+          when 'i'
+           (@weight/(@height**2))*703
+          when 'm'
+            @weight =params[:weight].to_f
+            @height = params[:height].to_f
+            (@weight/(@height**2))
+          else
+            puts "invalid option"
+        end
+
+erb :bmi_calc
+end
+
+get '/mortgage_calc' do
+  if params[:p]
+  @p = params[:p].to_f
+  @i = (params[:i].to_f / 12)
+  @n = params[:n].to_i
+  @x = (1+@i)**@n
+
+  @result = (@p*@i*@x)/(@x-1)
   end
 
-  get '/name/:first' do
-    "hello, #{params[:first]}"
+  erb :mortgage_calc
   end
 
-  get '/name/:first/:last/:age' do
-    "Your name is #{params[:first]} #{params[:last]}, you are #{params[:age]} years old."
-end
+get '/mileage_calc' do
+  if params[:p]
+  @distance = params[:distance].to_f || 0
+  @mpg = params[:mpg].to_f || 0
+  @cost = params[:cost].to_f || 0
+  @speed = params[:speed].to_f || 0
+  @hours = nil
+  if @speed > 60
+    @mpg = @mpg - ((@speed - 60) * 2)
+  end
 
-get '/multiply/:x/:y' do
-  @result = params[:x].to_f * params[:y].to_f
-  #'the result is: #{result}'
-  erb :calc
+    @hours = (@distance.to_f / @speed.to_f).round(2)
+  @cost_trip = ((@distance/@mpg)*@cost).round(2)
 end
+  erb :mileage_calc
+  end
 
-get 'divide/:x/:y' do
-  @result = params[:x].to_f / params[:y].to_f
-  #{}"the result is: #{result}"
-  erb :calc
-end
+
+
+
